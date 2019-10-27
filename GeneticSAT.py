52d5a8367a55dee13d54ee74a1b9a6ce925f4ca1
crin <37940924+crinuyi@users.noreply.github.com>
2019-10-27 19:26:25 +0100

    added fitness and check_disjunction

--- GeneticSAT.py
+++ GeneticSAT.py
@@ -1,6 +1,16 @@
+def check_disjunction(clause):
+    result = 0
+    for element_index in range(len(clause)):
+        result |= clause[element_index]
+    return result
+
+
 class GeneticSAT:
-    def __init__(self, population):
-        self.population = population
+    def __init__(self, formula):
+        self.formula = formula
 
     def fitness(self, chromosome):
-        pass
+        result = 0
+        for clause in self.formula:
+            result += check_disjunction(clause)
+        return result
