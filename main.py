7a6872d8c4aa8c498e0e8682e65b3df8f1cdaeca
crin <37940924+crinuyi@users.noreply.github.com>
2019-10-27 20:50:25 +0100

    running algorithm in main

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from GeneticSAT import GeneticSAT
+
+
 def main():
     formula = [
         [-1, 2, 4],
@@ -9,6 +12,9 @@ def main():
         [-1, 3, -4],
         [1, 2, 3]
     ]
+    genetic_sat = GeneticSAT(formula, 200, 500)
+    genetic_sat.run()
+    print(genetic_sat.log)
 
 
 if __name__ == '__main__':
